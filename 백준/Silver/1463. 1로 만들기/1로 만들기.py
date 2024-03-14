79c3c818c2c71cb7d421bd7d29218fb357e8bc4f
김동찬 <129833370+dongchandev@users.noreply.github.com>
2024-03-14 14:01:04 +0900

    [Silver III] Title: 1로 만들기, Time: 572 ms, Memory: 38932 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,18 @@
+import sys
+
+x = int(sys.stdin.readline().rstrip())
+
+dp = [0]*(x+1)
+for i in range(2, x+1):
+
+    dp[i] = dp[i-1]+1
+
+    if i % 2 == 0 and i % 3 == 0:
+        dp[i] = min(dp[(i+1)//2]+1, dp[(i+1)//3]+1)
+
+    elif i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
+    elif i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+
+print(dp[x])
