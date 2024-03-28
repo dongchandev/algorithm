60f084f941a67dacdfda460e64751351c979efc2
김동찬 <129833370+dongchandev@users.noreply.github.com>
2024-03-28 22:18:24 +0900

    [Silver III] Title: 프린터 큐, Time: 68 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+t = int(input())
+queue=deque([])
+for i in range(t):
+    n, m = map(int, input().split())
+    queue = deque(list(map(int, sys.stdin.readline().split())))
+    count = 0
+    while queue:
+        best = max(queue)  
+        front = queue.popleft() 
+        m -= 1 
+
+        if best == front: 
+            count += 1 
+            if m < 0: 
+                print(count)
+                break
+
+        else:   
+            queue.append(front) 
+            if m < 0 :  
+                m = len(queue) - 1 
