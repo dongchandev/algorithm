722d5508b2fa17bb81cb324117029c734c794cf5
김동찬 <129833370+dongchandev@users.noreply.github.com>
2024-06-23 21:56:41 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    s = list(s)
+    for i in range(len(s)):
+        if(i==0 or s[i-1]==' '):
+            s[i]=s[i].upper()
+        else:
+            s[i]=s[i].lower()
+    return "".join(s)
