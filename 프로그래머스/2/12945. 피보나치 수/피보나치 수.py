07e888c8af40b94dbe2d37a04c69438d06c29719
김동찬 <129833370+dongchandev@users.noreply.github.com>
2025-03-21 11:07:28 +0900

    [level 2] Title: 피보나치 수, Time: 181.25 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,8 +1,5 @@
 def solution(n):
-    b, a, s = 0, 1, 0
-    for i in range(n-1):
-        s = b + a
-        b = a
-        a = s
-
-    return s % 1234567
+    a,b = 0,1
+    for i in range(n):
+        a,b = b,a+b
+    return a % 1234567
