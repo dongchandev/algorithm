b8208f3ad76587568165b434ae2068dd2df6368e
김동찬 <129833370+dongchandev@users.noreply.github.com>
2025-07-03 10:11:58 +0900

    [level 2] Title: 기능개발, Time: 0.57 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,16 +1,13 @@
 def solution(progresses, speeds):
     answer = []
-    while progresses :
+    while progresses:
         cnt = 0
         while progresses and progresses[0] >= 100:
-            cnt+=1
+            cnt += 1
             progresses.pop(0)
             speeds.pop(0)
-
+        
         for i in range(len(progresses)):
-            progresses[i] += speeds[i]
-
-        if cnt:
-            answer.append(cnt)
-    
+            progresses[i] = progresses[i]+speeds[i]
+        if cnt: answer.append(cnt)
     return answer
